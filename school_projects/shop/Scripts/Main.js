04f289d96f575a1c7617deabdc9cbab9bc9949ce
Rokuazery <Rokuro81@gmail.com>
2022-12-09 14:42:21 +0700

    Better Navigation for mobile
    -Added tugas info in About page

--- school_projects/shop/Scripts/Main.js
+++ school_projects/shop/Scripts/Main.js
@@ -1,38 +1,71 @@
-
 const navigationToggle = document.querySelector('.nav-burger-menu-toggle');
 const navSideBar = document.querySelector('.nav-side-bar-darken');
 const navInnerSideBar = document.querySelector('.nav-side-bar-inner');
-const cartNavs = document.querySelectorAll("#cart");
+const navigationBar = document.getElementById('navbar');
+var cartNavs = null;
+var cartsTotal = null;
 
-const cartsTotal = document.querySelectorAll('#cartTotal');
-
-// const addToCartButtons = document.querySelectorAll('#addToCart');
+var navigationMenus = [
+    {name: "Home", path: "Home.html"},
+    {name: "Products", path: "Products.html"},
+    {name: "Contact", path: "Contact.html"},
+    {name: "About", path: "About.html"}
+];
 
 var cartItems = new Array();
-// export {cartCount} ;
 
 function onLoad(){
+    var navigationDropdown = document.querySelector('.navigation-menu-dropdown');
+
+    var dropdownItems ="";
+    var currentBody = document.body;
+    var currentPage = currentBody.getAttribute('current-page');
+    var currentPageDisplay = document.getElementById('current-page');
+    currentPageDisplay.innerHTML = currentPage;
+
+    navigationMenus.forEach(navMenu =>{
+        if(currentPage == navMenu.name){
+            dropdownItems += `<li><a class="dropdown-item active">${navMenu.name}</a></li>`;
+            return;
+        }
+
+        dropdownItems += `<li><a class="dropdown-item" href="${navMenu.path}">${navMenu.name}</a></li>`;
+    });
+
+    var navigationMenuItems = "";
+    navigationMenus.reverse().forEach(navMenu =>{
+        if(currentPage == navMenu.name){
+            navigationMenuItems += `<a class="nav-a active" href="${navMenu.path}">${navMenu.name}</a>`;
+            return;
+        }
+        navigationMenuItems += `<a class="nav-a" href="${navMenu.path}">${navMenu.name}</a>`;
+    });
+
+    navigationBar.innerHTML += navigationMenuItems;
+    navigationDropdown.innerHTML += dropdownItems;
+
+    cartNavs = document.querySelectorAll("#cart");
+    cartsTotal = document.querySelectorAll('#cartTotal');
+
     var cartItemsTemp = localStorage.getItem('cartItems');
+
     if(cartItemsTemp && cartItemsTemp.length > 0){
         cartItems = JSON.parse(cartItemsTemp);
 
-        cartsTotal.forEach(cartTotal => {
+            cartsTotal.forEach(cartTotal => {
             cartTotal.innerHTML = cartItems.length;
         });
     }
-}
 
-// Clear cart
-cartNavs.forEach(cartNav=>{
-    cartNav.addEventListener('click', ()=> {
-        cartItems = [];
-        cartsTotal.forEach(cartTotal => {
-            cartTotal.innerHTML = cartItems.length;
+    cartNavs.forEach(cartNav=>{
+        cartNav.addEventListener('click', ()=> {
+            console.log("cart cleared");
+            cartItems = [];
+            cartsTotal.forEach(cartTotal => cartTotal.innerHTML = cartItems.length);
+            localStorage.setItem('cartItems', JSON.stringify(cartItems));
         });
-        localStorage.setItem('cartItems', JSON.stringify(cartItems));
     });
-});
-
+}
 
 navSideBar.addEventListener('click', ()=> {
     setMainNavVisibility(false);
@@ -46,7 +79,7 @@ navigationToggle.addEventListener('click', ()=> {
     //     isFirstTime = true;
     // }
 
-    if(visibility == "false" || !visibility) {
+    if(visibility == "false" || !visibility){
         setMainNavVisibility(true);
     } else{
         setMainNavVisibility(false);
